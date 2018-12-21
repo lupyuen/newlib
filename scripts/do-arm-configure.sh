9fa700521b7246bd017b9e1cf310abee54676ead
lupyuen <luppy@appkaki.com>
2018-12-21 13:50:56 +0800

    Fixing ninja

--- scripts/do-arm-configure.sh
+++ scripts/do-arm-configure.sh
@@ -15,3 +15,25 @@ meson $DIR \
     -Dlibdir=lib/newlib-nano/$ARCH/lib \
     --cross-file $DIR/cross-$ARCH.txt \
     --buildtype plain
+
+# Build only Cortex-M: thumb/v7-m.  Skip v7e-m, which is for Cortex-M4
+ninja \
+    newlib/libc/machine/arm/libmachinethumb_v7_m.a \
+    newlib/libc/argz/libargzthumb_v7_m.a \
+    newlib/libc/ctype/libctypethumb_v7_m.a \
+    newlib/libc/errno/liberrnothumb_v7_m.a \
+    newlib/libc/iconv/libiconvthumb_v7_m.a \
+    newlib/libc/misc/libmiscthumb_v7_m.a \
+    newlib/libc/search/libsearchthumb_v7_m.a \
+    newlib/libc/signal/libsignalthumb_v7_m.a \
+    newlib/libc/ssp/libsspthumb_v7_m.a \
+    newlib/libc/stdlib/libstdlibthumb_v7_m.a \
+    newlib/libc/string/libstringthumb_v7_m.a \
+    newlib/libc/time/libtimethumb_v7_m.a \
+    newlib/libc/xdr/libxdrthumb_v7_m.a \
+    newlib/libc/locale/liblocalethumb_v7_m.a \
+    newlib/libc/tinystdio/libtinystdiothumb_v7_m.a \
+    newlib/libc/libthumb_v7_m/libc.a \
+    newlib/libm/machine/arm/libmachinethumb_v7_m.a \
+    newlib/libm/common/libcommonthumb_v7_m.a
+
